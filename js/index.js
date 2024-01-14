81ced96e2fd3984071d368ba4b6b18bdf6d1415e
nfroidure <nicolas.froidure@gmail.com>
2024-01-14 18:45:50 +0000

    Deploying to gh-pages from @ nfroidure/blog@9c1b23d3faa9f7598aefdcc5fffa193f2100091b 🚀

--- js/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'; // The following module is dynamically generated
-// TODO: Cleanup that shit
-
-var searchIndex = require('./search.index'); // eslint-disable-line
-
-
-module.exports = searchIndex;
